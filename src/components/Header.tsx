1c6cc30d1db89e747bc09eb8d6685732424bc5b9
Sheldon Odhiambo <sheldonodhiuma04@gmail.com>
2026-06-07 09:35:43 +0300

    feat

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
     <header className="fixed top-0 w-full z-50 flex justify-center items-center px-4 h-16 bg-black border-b border-gray-800">
       <div className="flex items-center gap-2">
         <ShieldCheck className="text-white w-6 h-6" />
-        <span className="text-2xl font-bold tracking-tight text-white">Alertify</span>
+        <span className="text-2xl font-bold tracking-tight text-white">SAFESYNC</span>
       </div>
     </header>
   );
